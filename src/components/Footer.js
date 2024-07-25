f27bbb05970750487a33b36a1fba48ffeeadcf0c
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-07-25 23:21:28 +0200

    Style footer and change space between elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,24 @@
 function Footer() {
   return (
-    <footer className="d-flex flex-wrap justify-content-between align-items-center p-3 my-4 border-top mt-auto">
+    <footer
+      id="footer"
+      className="d-flex flex-wrap justify-content-between align-items-center p-3 border-top mt-auto"
+    >
       <div className="col-md-4 d-flex align-items-center">
-        <span className="text-muted">
+        <span className="footer-text">
           &copy; {new Date().getFullYear()} wkktoria
         </span>
       </div>
 
       <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
         <li className="ms-3">
-          <a className="text-muted" href="https://github.com/wkktoria">
+          <a className="footer-text" href="https://github.com/wkktoria">
             <i className="bi bi-github"></i>
           </a>
         </li>
         <li className="ms-3">
           <a
-            className="text-muted"
+            className="footer-text"
             href="https://www.linkedin.com/in/wiktoria-peretiatkowicz"
           >
             <i className="bi bi-linkedin"></i>
